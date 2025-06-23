7170d91feac0a6e2f6c350a81fd04ca9dc5bd53e
Ale <alearaher@gmail.com>
2025-06-23 11:31:33 -0400

    cleaning up

--- content.js
+++ content.js
@@ -1,16 +1,7 @@
 // This script runs on the webpage the user is currently viewing.
 (() => {
 
-    let toggle = true;
-    // need to fetch
-
     console.log("I am in the content script!");
-
-   
-
-
-
-
 
 })()
 
@@ -18,8 +9,17 @@
  chrome.runtime.onMessage.addListener(function(message, sender, senderResponse){
 
         console.log(message);
-        if(message.message == "turn_blue"){
-            console.log("Turning blue....")
-            document.body.style.backgroundColor = "blue";
+        if(message.message == "start_recording"){
+            console.log("Recording DOM elements clicked");
+
+
+        }else if(message.message == "stop_recording"){
+            console.log("Recording has stopped");
+            console.log("Saving to macro...");
+            //Reload page
+            //go over list and click elements as follows
+            //document.getElementById('elementID').click();
         }
     });
+
+    
--- popup.js
+++ popup.js
@@ -9,20 +9,31 @@ async function getActiveTabURL() {
 
 document.addEventListener('DOMContentLoaded', async () => {
   const messageElement = document.getElementById('message');
-  const myButton = document.getElementById('myButton');
+  const startButton = document.getElementById('startButton');
+  const stopButton = document.getElementById('stopButton');
 
   // Display a temporary message
   messageElement.textContent = "Extension is mounted!";
   const activeTab = await getActiveTabURL();
 
   // Example: Save something to storage on button click
-  myButton.addEventListener('click',  () => {
+  startButton.addEventListener('click',  () => {
     chrome.storage.local.set({ 'popupClicked': true }, () => {
       console.log('Button click state saved to storage.');
       messageElement.textContent = "Button clicked and state saved!";
-      chrome.tabs.sendMessage(activeTab.id, {message: "turn_blue"});
+      chrome.tabs.sendMessage(activeTab.id, {message: "start_record"});
     });
   });
+
+
+  stopButton.addEventListener('click',  () => {
+    chrome.storage.local.set({ 'popupClicked': true }, () => {
+      console.log('Button click state saved to storage.');
+      messageElement.textContent = "Button clicked and state saved!";
+      chrome.tabs.sendMessage(activeTab.id, {message: "stop_record"});
+    });
+  });
+
 
   // Example: Retrieve something from storage on load
   chrome.storage.local.get(['popupClicked'], (result) => {
